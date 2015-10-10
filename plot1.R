c519a504c4ff40100b1c13a493fdc75facf42b11
mfairless <mfairless@Michael-Fairlesss-MacBook-Pro.local>
2015-10-10 18:20:33 -0400

    Entered wrong width/height
    looking at the effect of larger width/height settings.  Fixed.

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ data <- read.table(globalactivedata, header=TRUE, sep=";", stringsAsFactors=FALS
  subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
  globalActivePower <- as.numeric(subSetData$Global_active_power)
 ## Generate png file
- png("plot1.png", width=800, height=800)
+ png("plot1.png", width=480, height=480)
 ## Generate histogram 
  hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")  
  dev.off()
